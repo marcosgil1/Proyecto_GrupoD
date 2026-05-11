426f5b62447b7aecf20bd1eaf5b6e400459b2ee1
marcosgil1 <marcosgil7115@gmail.com>
2026-05-11 09:41:29 +0200

    #12

---
+++ proyecto(javadoc)/proyecto(javadoc)/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"clases"},{"l":"controlador"},{"l":"DAO"},{"l":"GUI"}];updateSearchResults();
---
+++ proyecto(javadoc)/proyecto(javadoc)/src/clases/Recepcionista.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class Recepcionista extends Usuario{
+
+	public Recepcionista(String dni, String nombre, String apellidos, String telefono, String email, String fechaAlta,
+			String estado, String contrasena) {
+		super(dni, nombre, apellidos, telefono, email, fechaAlta, estado, contrasena);
+	}
+
+}
